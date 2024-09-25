9b0be53b7a2c90943d2da7630c77fdc0ef51e815
Andrew Cline <acline@precisionplanting.com>
2024-09-25 11:39:18 -0500

    fix: macos-theme

--- macos.nix
+++ macos.nix
@@ -1,55 +1,11 @@
 { pkgs, pkgs-unstable, ... }:
 
-let
-  colors = import ./colors.nix { };
-
-in
 {
   imports = [ ];
 
   nixpkgs.config.allowUnfree = true;
   stylix.enable = true;
-  stylix.base16Scheme = {
-    # -- base00 - Default Background
-    base00 = "#15181A";
-    # -- base01 - Lighter Background (Used for status bars, line number and folding marks)
-    base01 = "#242a2d";
-    # -- base02 - Selection Background
-    base02 = "#3C4344";
-    # -- base03 - Comments, Invisibles, Line Highlighting
-    base03 = "#737A82";
-    # -- base04 - Dark Foreground (Used for status bars)
-    base04 = "#3C4344";
-    # -- base05 - Default Foreground, Caret, Delimiters, Operators
-    base05 = "#E1E3E6";
-    # -- base06 - Light Foreground (Not often used)
-    base06 = "#eceff3";
-    # -- base07 - Light Background (Not often used)
-    base07 = "#eceff3";
-    # -- base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-    base08 = "#df9a98";
-    # -- base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
-    base09 = "#FFA474";
-    # -- base0A - Classes, Markup Bold, Search Text Background
-    base0A = "#FFD187";
-    # -- base0B - Strings, Inherited Class, Markup Code, Diff Inserted
-    base0B = "#93C394";
-    # -- base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
-    base0C = "#87afaf";
-    # -- base0D - Functions, Methods, Attribute IDs, Headings
-    base0D = "#80B6D7";
-    # -- base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
-    base0E = "#dfbdbc";
-    # -- base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
-    base0F = "#e9dbc2";
-    base10 = "#242a2d";
-    base11 = "#15181A";
-    base12 = "#e07798";
-    base13 = "#729973";
-    base14 = "#5f8787";
-    base15 = "#5e90af";
-    base16 = "#af8787";
-  };
+  stylix.base16Scheme = ./base16-theme.nix;
   stylix.image = ./red-blue-wall.jpg;
 
   stylix.targets.neovim.enable = false;
@@ -182,12 +138,11 @@ in
     extraConfig = ''
 
       # statusbar
-      set -g status-position bottom
-      set -g status-justify left
-      set -g status-style 'bg=#15181A fg=colour7 dim'
-      set -g status-left "" 
-      set -g status-right "#S"
-      setw -g window-status-format ' #I:#W#F '
+      # set -g status-position bottom
+      # set -g status-justify left
+      # set -g status-left "" 
+      # set -g status-right "#S"
+      # setw -g window-status-format ' #I:#W#F '
 
       set-option -a terminal-features 'xterm-256color:RGB'
     '';
