6e45112fb3818ef3ab60aa05a53d3a3cf14ddeda
Andrew Cline <acline@precisionplanting.com>
2024-09-25 11:36:44 -0500

    feat: add base16 files

---
+++ base16-theme.nix
@@ -0,0 +1,41 @@
+{
+  # -- base00 - Default Background
+  base00 = "#3a3e41";
+  # -- base01 - Lighter Background (Used for status bars, line number and folding marks)
+  base01 = "#242a2d";
+  # -- base02 - Selection Background
+  base02 = "#3C4344";
+  # -- base03 - Comments, Invisibles, Line Highlighting
+  base03 = "#737A82";
+  # -- base04 - Dark Foreground (Used for status bars)
+  base04 = "#3C4344";
+  # -- base05 - Default Foreground, Caret, Delimiters, Operators
+  base05 = "#E1E3E6";
+  # -- base06 - Light Foreground (Not often used)
+  base06 = "#eceff3";
+  # -- base07 - Light Background (Not often used)
+  base07 = "#eceff3";
+  # -- base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+  base08 = "#df9a98";
+  # -- base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  base09 = "#FFA474";
+  # -- base0A - Classes, Markup Bold, Search Text Background
+  base0A = "#FFD187";
+  # -- base0B - Strings, Inherited Class, Markup Code, Diff Inserted
+  base0B = "#93C394";
+  # -- base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
+  base0C = "#87afaf";
+  # -- base0D - Functions, Methods, Attribute IDs, Headings
+  base0D = "#80B6D7";
+  # -- base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
+  base0E = "#dfbdbc";
+  # -- base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+  base0F = "#e9dbc2";
+  base10 = "#242a2d";
+  base11 = "#15181A";
+  base12 = "#e07798";
+  base13 = "#729973";
+  base14 = "#5f8787";
+  base15 = "#5e90af";
+  base16 = "#af8787";
+}
